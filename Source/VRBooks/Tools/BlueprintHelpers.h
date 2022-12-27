98ad8c2968349e2eb70acb168abbef2d78e1c684
Ryan Ash <scarletspark@gmail.com>
2022-12-27 07:46:08 +0100

    cpp project generated

---
+++ Source/VRBooks/Tools/BlueprintHelpers.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BlueprintHelpers.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRBOOKS_API UBlueprintHelpers : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
